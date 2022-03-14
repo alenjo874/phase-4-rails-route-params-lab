e0d884adad3679c0712a7b9ad32df103cdccbe96
Alen Jo <alenjo874@gmail.com>
2022-03-14 17:36:59 -0400

    commit tree 3c2af566687fe0e576b0901cceebb3aa5eeb1a6b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,13 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.find_by(first_name: params[:first], last_name: params[:last])
     render json: students
+  end
+
+  def show
+    show_student = Student.find(params[:id])
+    render json: show_student
   end
 
 end
